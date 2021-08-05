ee30925748ed16cfd46563322ee93e7bf8dbf7be
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-05 22:15:27 +0200

    2.5 Parser write() method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,6 +10,10 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(self, path: Path):
+    def read(self, path: Path) -> str:
         with open(path, 'r') as file:
             return file.read()
+
+    def write(self, path: Path, dest: Path, content: str, ext: str = '.html'):
+        full_path = dest / path.with_suffix(ext).name
+        # with open()
