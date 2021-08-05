9f366c0e0be6da091ad1991105a971f910b4b275
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-05 22:21:28 +0200

    2.6 Open file for write

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,7 +2,8 @@ from typing import ClassVar, List
 from pathlib import Path
 
 class Parser:
-    extensions: List[str] = list()
+    def __init__(self) -> None:
+        self.extensions: List[str] = list()
 
     def validate_extension(self, extension: str) -> bool:
         return extension in self.extensions
