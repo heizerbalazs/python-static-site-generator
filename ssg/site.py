05d0414e100763130bfa4d46f68a21d668cdbe5c
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-05 21:06:14 +0200

    4 Make a directory

--- ssg/site.py
+++ ssg/site.py
@@ -6,4 +6,5 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path: Path):
-        destination = self.dest / path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
