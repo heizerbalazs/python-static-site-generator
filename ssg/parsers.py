57855e6fce7af1d076b7f60d1df7735c5188685b
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-05 21:28:04 +0200

    2.2 validate extensions

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 class Parser:
     extensions: List[str] = list()
+
+    def validate_extension(self, extension: str) -> bool:
+        return extension in self.extensions
