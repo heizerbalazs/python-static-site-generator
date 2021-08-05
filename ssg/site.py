a8a018575f341dd090708ae138a2a105aa5d6113
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-05 21:04:24 +0200

    3 Find root directory

--- ssg/site.py
+++ ssg/site.py
@@ -4,3 +4,6 @@ class Site:
     def __init__(self, source: str, dest: str) -> None:
         self.source = Path(source)
         self.dest = Path(dest)
+
+    def create_dir(self, path: Path):
+        destination = self.dest / path.relative_to(self.source)
