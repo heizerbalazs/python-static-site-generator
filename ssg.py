31e456754478adce4c176e5c80ff9596f2c8ae7b
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-06 09:45:59 +0200

    2.10 Parser configuration

--- ssg.py
+++ ssg.py
@@ -1,12 +1,16 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
+
 
 def main(source: str = 'content', dest: str = 'dist') -> None:
     config = {
         'source': source,
-        'dest': dest
+        'dest': dest,
+        'parsers': [ssg.parsers.ResourceParser()]
     }
 
     Site(**config).build()
+
 
 typer.run(main)
