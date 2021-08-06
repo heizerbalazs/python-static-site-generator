5da4045d35d19158c71854f4d792dfb551855b4d
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-06 11:54:32 +0200

    2.2 Content class

--- ssg/content.py
+++ ssg/content.py
@@ -4,5 +4,5 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = '^(?:-|\+){3}\s*$'
+    __delimiter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
