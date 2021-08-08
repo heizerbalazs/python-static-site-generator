d55f1bd3e07f27709016ab75a46adfd13386bf2f
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-08 08:54:05 +0200

    4.6 ReStructuredTextPareser parse() method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -49,3 +49,6 @@ class MarkdownParser(Parser):
 
 class ReStructuredTextParser(Parser):
     extensions = ['.rst']
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
